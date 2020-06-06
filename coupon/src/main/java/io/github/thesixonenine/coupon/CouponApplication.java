db467f28e14cc8eef5b4d2681b2c4c9272b7daf7
thesixonenine <huyang1994822@gmail.com>
2020-06-07 00:52:54 +0800

    add bootstrap.yml

--- coupon/src/main/java/io/github/thesixonenine/coupon/CouponApplication.java
+++ coupon/src/main/java/io/github/thesixonenine/coupon/CouponApplication.java
@@ -1,15 +1,28 @@
 package io.github.thesixonenine.coupon;
 
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RefreshScope
+@RestController
 @MapperScan(value = "io.github.thesixonenine.coupon.dao")
 @SpringBootApplication
 public class CouponApplication {
+
+    @Value("${coupon.expire.time}")
+    private Integer expireTime;
 
     public static void main(String[] args) {
         SpringApplication.run(CouponApplication.class, args);
     }
 
+    @GetMapping(value = "/")
+    public void test() {
+        System.out.println("过期时间: " + expireTime);
+    }
 }
