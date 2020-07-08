f07ec6fd2c491a17c54fdcfb42b086d589d44f4c
thesixonenine <huyang1994822@gmail.com>
2020-07-09 00:23:14 +0800

    增加线程池配置

---
+++ product/src/main/java/io/github/thesixonenine/product/config/ThreadPoolConfigProperties.java
@@ -0,0 +1,20 @@
+package io.github.thesixonenine.product.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author Simple
+ * @version 1.0
+ * @date 2020/07/09 0:05
+ * @since 1.0
+ */
+@Data
+@Component
+@ConfigurationProperties(prefix = "thread")
+public class ThreadPoolConfigProperties {
+    private int corePoolSize;
+    private int maximumPoolSize;
+    private long keepAliveTime;
+}
