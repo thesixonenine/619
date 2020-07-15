eec54f9e4651682985a02f26c56f844d8d0c9260
thesixonenine <huyang1994822@gmail.com>
2020-07-16 00:17:54 +0800

    登录功能-已登录的再次访问登录页直接去首页

--- search/src/main/java/io/github/thesixonenine/search/SearchApplication.java
+++ search/src/main/java/io/github/thesixonenine/search/SearchApplication.java
@@ -4,8 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
+@EnableRedisHttpSession
 @EnableSwagger2
 @EnableFeignClients(basePackages = {"io.github.thesixonenine"})
 @RefreshScope
