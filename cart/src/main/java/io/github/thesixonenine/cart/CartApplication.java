b377a6eab0a1e7f2448ce682bde5112d843c4795
Simple <huyang1994822@gmail.com>
2020-09-10 21:37:26 +0800

    增加购物车模块

---
+++ cart/src/main/java/io/github/thesixonenine/cart/CartApplication.java
@@ -0,0 +1,29 @@
+package io.github.thesixonenine.cart;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@EnableSwagger2
+@EnableFeignClients(basePackages = {"io.github.thesixonenine"})
+@RefreshScope
+@Controller
+@MapperScan(value = "io.github.thesixonenine.cart.dao")
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+public class CartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CartApplication.class, args);
+    }
+
+    @RequestMapping(value = "")
+    public String index(){
+        return "success";
+    }
+}
