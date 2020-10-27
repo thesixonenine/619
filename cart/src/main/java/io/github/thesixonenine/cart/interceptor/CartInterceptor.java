7f7ea792c92a1eeb6b16355ff15f179d8616e394
Simple <huyang1994822@gmail.com>
2020-10-27 23:31:08 +0800

    使用ThreadLocal共享变量的方式配置临时用户

--- cart/src/main/java/io/github/thesixonenine/cart/interceptor/CartInterceptor.java
+++ cart/src/main/java/io/github/thesixonenine/cart/interceptor/CartInterceptor.java
@@ -11,8 +11,6 @@ import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-import java.time.LocalDateTime;
-import java.time.ZoneOffset;
 import java.util.Objects;
 import java.util.UUID;
 
