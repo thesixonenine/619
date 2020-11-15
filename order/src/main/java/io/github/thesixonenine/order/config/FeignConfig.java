d6dae9a4de9d13d7fc1c379ec16d4dc7c76ea064
Simple <huyang1994822@gmail.com>
2020-11-15 20:21:31 +0800

    269 - feign远程异步调用丢失请求头的问题 通过将主线程的RequestAttributes复制到异步线程中来处理

--- order/src/main/java/io/github/thesixonenine/order/config/FeignConfig.java
+++ order/src/main/java/io/github/thesixonenine/order/config/FeignConfig.java
@@ -8,6 +8,7 @@ import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.Objects;
 
 /**
  * @author Simple
@@ -23,11 +24,15 @@ public class FeignConfig {
         return new RequestInterceptor() {
             @Override
             public void apply(RequestTemplate requestTemplate) {
+                // 如果不是http请求进来的线程, 这里的attributes就是null
+                // 例如http请求进来后, 开启异步线程来执行远程调用
                 ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
-                HttpServletRequest request = attributes.getRequest();
-                // 同步header中的Cookie, 因为这里有用户登录信息
-                String cookie = request.getHeader("Cookie");
-                requestTemplate.header("Cookie", cookie);
+                if (Objects.nonNull(attributes)) {
+                    HttpServletRequest request = attributes.getRequest();
+                    // 同步header中的Cookie, 因为这里有用户登录信息
+                    String cookie = request.getHeader("Cookie");
+                    requestTemplate.header("Cookie", cookie);
+                }
             }
         };
     }
--- order/src/main/java/io/github/thesixonenine/order/service/impl/OrderServiceImpl.java
+++ order/src/main/java/io/github/thesixonenine/order/service/impl/OrderServiceImpl.java
@@ -20,10 +20,15 @@ import io.github.thesixonenine.order.vo.OrderConfirmVO;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.context.request.RequestAttributes;
+import org.springframework.web.context.request.RequestContextHolder;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ThreadPoolExecutor;
 
 
 @Service("orderService")
@@ -34,6 +39,8 @@ public class OrderServiceImpl extends ServiceImpl<OrderDao, OrderEntity> impleme
     private ICartController cartController;
     @Autowired
     private MemberController memberController;
+    @Autowired
+    private ThreadPoolExecutor threadPoolExecutor;
 
     @Override
     public PageUtils queryPage(Map<String, Object> params) {
@@ -47,6 +54,64 @@ public class OrderServiceImpl extends ServiceImpl<OrderDao, OrderEntity> impleme
 
     @Override
     public OrderConfirmVO confirmOrder() {
+        return confirmOrderV2();
+    }
+
+    private OrderConfirmVO confirmOrderV2() {
+        Long memberId = LoginInterceptor.threadLocal.get().getLeft();
+        OrderConfirmVO orderConfirmVO = new OrderConfirmVO();
+        // 改成异步后的问题:
+        // feign在调用远程接口之前会应用我们自定义的Interceptor(io.github.thesixonenine.order.config.FeignConfig.requestInterceptor)
+        // 以此来带上request中的Cookie中的会员信息, 因为主线程中有
+        // 但是改成异步后, 异步线程中是没有HttpServletRequest的
+        // 所以((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest()是get不到HttpServletRequest的
+        // 解决办法就是将主线程的copy过去
+
+        RequestAttributes attributes = RequestContextHolder.getRequestAttributes();
+
+        CompletableFuture<Void> getAddr = CompletableFuture.runAsync(() -> {
+            List<MemberReceiveAddressEntity> addressEntityList = memberReceiveAddressController.getByMemberId(memberId);
+
+            List<MemberReceiveAddressVO> addressList = new ArrayList<>(addressEntityList.size());
+            for (MemberReceiveAddressEntity memberReceiveAddressEntity : addressEntityList) {
+                MemberReceiveAddressVO vo = new MemberReceiveAddressVO();
+                BeanUtils.copyProperties(memberReceiveAddressEntity, vo);
+                addressList.add(vo);
+            }
+            orderConfirmVO.setAddressList(addressList);
+        });
+        CompletableFuture<Void> getCartItemList = CompletableFuture.runAsync(() -> {
+
+            RequestContextHolder.setRequestAttributes(attributes);
+
+            // 调用远程服务获取当前的购物车
+            // 注意这里涉及到了用户登录状态的传递
+            // 也就是说不能直接使用feign提供的restTemplate来进行调用, 这样会丢失请求头header
+            // 需要添加feign请求拦截器, 加上请求头
+            // feign.SynchronousMethodHandler.executeAndDecode
+            List<CartItem> cartItem = cartController.getCurrentCartItem();
+
+            List<CartItemVO> cartItemList = new ArrayList<>(cartItem.size());
+            for (CartItem item : cartItem) {
+                CartItemVO vo = new CartItemVO();
+                BeanUtils.copyProperties(item, vo);
+                cartItemList.add(vo);
+            }
+            orderConfirmVO.setCartItemList(cartItemList);
+        });
+        CompletableFuture<Void> getMember = CompletableFuture.runAsync(() -> {
+            orderConfirmVO.setIntegration(memberController.getById(memberId).getIntegration());
+        });
+
+        try {
+            CompletableFuture.allOf(getAddr, getCartItemList, getMember).get();
+        } catch (InterruptedException | ExecutionException ignored) {
+
+        }
+        return orderConfirmVO;
+    }
+
+    private OrderConfirmVO confirmOrderV1() {
         Long memberId = LoginInterceptor.threadLocal.get().getLeft();
         OrderConfirmVO orderConfirmVO = new OrderConfirmVO();
         List<MemberReceiveAddressEntity> addressEntityList = memberReceiveAddressController.getByMemberId(memberId);
